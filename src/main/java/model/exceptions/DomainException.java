e21ccd7f931a8b3127362ce4c320c7ca1d93184d
Yuri Siman <yurisimannogueira@gmail.com>
2020-05-21 16:29:37 -0300

    Criando Exceção - Model

---
+++ src/main/java/model/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+public class DomainException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String msg;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
